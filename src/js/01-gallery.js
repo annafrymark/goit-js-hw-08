4b0ce8d4d21363b18c10cab0adcabf83a168c42b
annafrymark <anna.frymark@gmail.com>
2023-01-07 23:30:45 +0100

    task gallery ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+galleryItems.forEach(function (galleryItem) {
+  const a = document.createElement('a');
+  a.classList.add('gallery__item');
+  a.setAttribute('href', galleryItem.original);
+  const img = document.createElement('img');
+  a.appendChild(img);
+  img.classList.add('gallery__image');
+  img.setAttribute('src', galleryItem.preview);
+  img.setAttribute('alt', galleryItem.description);
+  document.querySelector('div.gallery').appendChild(a);
+});
+
 console.log(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
