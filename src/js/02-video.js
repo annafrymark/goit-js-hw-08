f9fb73657e3228701d8bc354c081a9f6c0e054b6
annafrymark <anna.frymark@gmail.com>
2023-01-11 00:32:52 +0100

    task 2 - video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+console.log(localStorage.getItem('videoplayer-current-time'));
+
+if (localStorage.getItem('videoplayer-current-time') != null) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+const onTimeupdate = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds.toString());
+};
+
+player.on('timeupdate', onTimeupdate);
