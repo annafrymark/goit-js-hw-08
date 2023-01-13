d5bb65a0f65cee07b43c8ca5eb0effe039723f26
annafrymark <anna.frymark@gmail.com>
2023-01-13 18:59:44 +0100

    task 3 ready

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,37 @@
-document
-  .querySelector('form.feedback-form')
-  .addEventListener('input', event => {
+let formState = { email: '', message: '' };
+let emailField = document.querySelector('input[name="email"]');
+let messageField = document.querySelector('textarea[name="message"]');
+
+const throttle = require('lodash.throttle');
+
+if (localStorage.getItem('feedback-form-state') != null) {
+  formState = JSON.parse(localStorage.getItem('feedback-form-state'));
+}
+
+emailField.value = formState.email;
+messageField.value = formState.message;
+
+document.querySelector('form.feedback-form').addEventListener(
+  'input',
+  throttle(event => {
     localStorage.setItem(
       'feedback-form-state',
       JSON.stringify({
-        email: document.querySelector('input[name="email"]').value,
-        message: document.querySelector('textarea[name="message"]').value,
+        email: emailField.value,
+        message: messageField.value,
       })
     );
-    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  }, 500)
+);
+
+document
+  .querySelector('form.feedback-form')
+  .addEventListener('submit', event => {
+    event.preventDefault();
+    localStorage.clear();
+    formState.email = emailField.value;
+    formState.message = messageField.value;
+    console.log(formState);
+    emailField.value = '';
+    messageField.value = '';
   });
