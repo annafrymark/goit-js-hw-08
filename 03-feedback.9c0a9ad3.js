7e904adb86e28eed35bf3e922adbf89c06f0f3b5
annafrymark <110030398+annafrymark@users.noreply.github.com>
2023-01-13 18:00:24 +0000

    Deploying to gh-pages from @ annafrymark/goit-js-hw-08@d5bb65a0f65cee07b43c8ca5eb0effe039723f26 🚀

--- 03-feedback.9c0a9ad3.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("form.feedback-form").addEventListener("input",(e=>{localStorage.setItem("feedback-form-state",JSON.stringify({email:document.querySelector('input[name="email"]').value,message:document.querySelector('textarea[name="message"]').value})),console.log(JSON.parse(localStorage.getItem("feedback-form-state")))}));
-//# sourceMappingURL=03-feedback.9c0a9ad3.js.map
